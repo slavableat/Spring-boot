32ecc099e15f3e3ba925d9087d8a32e5e3f00242
Slava Alekseevets <s.alekseevets96@gmail.com>
2022-02-13 16:20:24 +0300

    configuring change

--- src/main/java/com/example/springboothibernate/Controller/BookController.java
+++ src/main/java/com/example/springboothibernate/Controller/BookController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200","http://localhost:4200"})
 @RestController
 public class BookController {
     @Autowired
